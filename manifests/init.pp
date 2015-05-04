238821114005bf47d188809a8112744051414954
root <root@lionssrv1.epfl.ch>
2015-05-04 15:53:11 +0200

    Integrate with PAM
    Do the same thing as "authconfig --enablesssd --enablesssdauth
    --updateall" using Puppet and a minimum of repeating. Debian support
    should be added.

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,4 +20,57 @@ class epfl_sso() {
     netgroup => ['files', 'sss'],
     sudoers => ['files', 'sss']
   }
+
+  # Mimic "authconfig --enablesssd --enablesssdauth --updateall" using
+  # https://forge.puppetlabs.com/herculesteam/augeasproviders_pam
+  # TODO: also support debian-style /etc/pam.d layout (common-{auth,account,password})
+  create_resources(pam,
+  {
+    'sss auth in system-auth' => { service => 'system-auth'},
+    'sss auth in password-auth' => { service => 'password-auth'}
+  },
+  {
+    ensure    => present,
+    type      => 'auth',
+    control   => 'sufficient',
+    module    => 'pam_sss.so',
+    arguments => 'use_first_pass',
+    position  => 'before *[type="auth" and module="pam_deny.so"]',
+  })
+  create_resources(pam,
+  {
+    'sss account in system-auth' => { service => 'system-auth'},
+    'sss account in password-auth' => { service => 'password-auth'}
+  },
+  {
+    ensure    => present,
+    type      => 'account',
+    control   => '[default=bad success=ok user_unknown=ignore]',
+    module    => 'pam_sss.so',
+    position  => 'before *[type="account" and module="pam_permit.so"]',
+  })
+  create_resources(pam,
+  {
+    'sss password in system-auth' => { service => 'system-auth'},
+    'sss password in password-auth' => { service => 'password-auth'}
+  },
+  {
+    ensure    => present,
+    type      => 'password',
+    control   => 'sufficient',
+    module    => 'pam_sss.so',
+    arguments => 'use_authtok',
+    position  => 'before *[type="password" and module="pam_deny.so"]',
+  })
+  create_resources(pam,
+  {
+    'sss session in system-auth' => { service => 'system-auth'},
+    'sss session in password-auth' => { service => 'password-auth'}
+  },
+  {
+    ensure    => present,
+    type      => 'session',
+    control   => 'optional',
+    module    => 'pam_sss.so',
+  })
 }
