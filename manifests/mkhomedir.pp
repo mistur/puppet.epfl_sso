bd21a0c0d52b97046269ab8a35ed37213fd1d0e4
root <root@ubuntusrv16.epfl.ch>
2016-04-15 19:15:58 +0200

    Support Debian-ish OSes for PAM

--- manifests/mkhomedir.pp
+++ manifests/mkhomedir.pp
@@ -42,57 +42,31 @@ class epfl_sso::mkhomedir() {
   }
 
   # Mimic "authconfig --enablemkhomedir"
-  if $lsbdistid == 'RedHat' {
-    create_resources(pam,
-    {
-      'mkhomedir session in system-auth' => { service => 'system-auth'},
-      'mkhomedir session in password-auth' => { service => 'password-auth'}
-    },
-    {
-      ensure    => present,
-      type      => 'session',
-      control   => 'optional',
-      module    => 'pam_mkhomedir.so',
-    })
-  }
-
-  # See https://wiki.debian.org/LDAP/PAM
-  # From https://help.ubuntu.com/lts/serverguide/sssd-ad.html:
-  # Home directories with pam_mkhomedir (optional)
-  #  When logging in using an Active Directory user account, it is likely that user has no home directory.
-  #  This can be fixed with pam_mkdhomedir.so, which will create the user's home directory on login.
-  #  Edit /etc/pam.d/common-session, and add this line directly after session required pam_unix.so:
-  #  `session    required    pam_mkhomedir.so skel=/etc/skel/ umask=0022`
-  if $lsbdistid == 'Ubuntu' {
-    $default_pam_session_lines = [
-      'session  [default=1] pam_permit.so',
-      'session requisite pam_deny.so',
-			'session required  pam_permit.so',
-			'session optional  pam_umask.so',
-			#'session required  pam_vas3.so create_homedir',
-			#'session requisite pam_vas3.so echo_return',
-			'session required  pam_unix.so',
-			'session required  pam_mkhomedir.so skel=/etc/skel/ umask=0022',
-		]
-
-		file { 'pam_session_lines':
-			ensure  => file,
-			path    => '/etc/pam.d/common-session',
-			content => template('epfl_sso/common-session.erb'),
-			owner   => 'root',
-			group   => 'root',
-			mode    => '0644',
-		}
-    create_resources(pam,
-    {
-      'mkhomedir session in system-auth' => { service => 'system-auth'},
-      'mkhomedir session in password-auth' => { service => 'password-auth'}
-    },
-    {
-      ensure    => present,
-      type      => 'session',
-      control   => 'optional',
-      module    => 'pam_mkhomedir.so',
-    })
+  case $::osfamily {
+    'RedHat': {
+      create_resources(pam,
+      {
+        'mkhomedir session in system-auth' => { service => 'system-auth'},
+        'mkhomedir session in password-auth' => { service => 'password-auth'}
+      },
+      {
+        ensure    => present,
+        type      => 'session',
+        control   => 'optional',
+        module    => 'pam_mkhomedir.so',
+      })
+    }
+    'Debian': {
+      create_resources(pam,
+      {
+        'mkhomedir session in common-auth' => { service => 'common-auth'},
+      },
+      {
+        ensure    => present,
+        type      => 'session',
+        control   => 'optional',
+        module    => 'pam_mkhomedir.so',
+      })
+    }
   }
 }
