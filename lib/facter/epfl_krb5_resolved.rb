4571ea707fb194bc306470fd1c7f4b733fc9a5d6
Dominique <dominique.quatravaux@epfl.ch>
2016-12-13 10:45:04 +0100

    Mask out failures because dig command is missing
    Tested by fail() (unfortunately puppet --facter doesn't work)

--- lib/facter/epfl_krb5_resolved.rb
+++ lib/facter/epfl_krb5_resolved.rb
@@ -4,12 +4,16 @@ $logger = Logger.new(STDERR)
 
 Facter.add('epfl_krb5_resolved') do
   setcode do
-    value = `dig -t srv _kerberos._tcp.intranet.epfl.ch`
-    # $logger.log(1, value)
-    if value == nil then
-      "undefined"
-    else 
-      !! value.index("IN\tSRV\t")
+    begin
+       value = `dig -t srv _kerberos._tcp.intranet.epfl.ch`
+       # $logger.log(1, value)
+       if value == nil then
+         "undefined"
+       else
+         !! value.index("IN\tSRV\t")
+       end
+    rescue Errno::ENOENT
+       "undefined"
     end
   end
 end
