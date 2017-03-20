3dd2e64ae5af615339a888edf877363e4b56c570
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-03-20 16:23:57 +0100

    One fact per PAM module to configure
    It looks like configuring pam_*.so modules in whole (as opposed to
    per PAM phase) is how both RedHat's authconfig and Ubuntu's
    pam-auth-update roll.

---
+++ lib/facter/pam.rb
@@ -0,0 +1,51 @@
+require 'logger'
+
+$logger = Logger.new(STDERR)
+
+def pam_file_of_distro(type)
+  return case Facter.value(:osfamily)
+         when 'RedHat'
+           "/etc/pam.d/system-#{type}-ac"
+         when 'Debian'
+           "/etc/pam.d/common-#{type}"
+         else
+           "/etc/pam.d/*-#{type}*"
+         end
+end
+
+def grep_in_pam_file(type, what)
+  pam_file = (type == "*") ? "/etc/pam.d/*" : pam_file_of_distro(type)
+  system("grep -q #{what} #{pam_file}")
+  $? == 0
+end
+  
+Facter.add('pam_sss_configured') do
+  setcode do
+    grep_in_pam_file("auth", "pam_sss")
+  end
+end
+
+Facter.add('pam_krb5_configured') do
+  setcode do
+    grep_in_pam_file("auth", "pam_krb5")
+  end
+end
+
+Facter.add('pam_mkhomedir_configured') do
+  setcode do
+    grep_in_pam_file("session", "pam_mkhomedir")
+  end
+end
+
+Facter.add('pam_access_configured') do
+  setcode do
+    grep_in_pam_file("account", "pam_access")
+  end
+end
+
+# It's a *bad* thing to have winbind, so look for it everywhere
+Facter.add('pam_winbind_configured') do
+  setcode do
+    grep_in_pam_file("*", "pam_winbind")
+  end
+end
