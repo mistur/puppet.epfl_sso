213a271ecacca69362891949a1522820c084672a
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-03-22 14:00:37 +0100

    Guard whole package to do nothing on Debian-ish

--- manifests/private/package_sources.pp
+++ manifests/private/package_sources.pp
@@ -1,11 +1,13 @@
 class epfl_sso::private::package_sources {
-  if ($::operatingsystem == "RedHat") {
-    exec { "subscription-manager repos --enable=rhel-${::osmajorrelease}-server-optional-rpms":
-      path => $::path,
-      unless => "rpm -q bsh || yum search bsh"
-    } -> Package["epel-release"]
-  }
-  package { "epel-release":
-    ensure => "present"
+  if ($::osfamily == "RedHat") {
+    if ($::operatingsystem == "RedHat") {
+      exec { "subscription-manager repos --enable=rhel-${::osmajorrelease}-server-optional-rpms":
+        path => $::path,
+        unless => "rpm -q bsh || yum search bsh"
+        } -> Package["epel-release"]
+    }
+    package { "epel-release":
+      ensure => "present"
+    }
   }
 }
